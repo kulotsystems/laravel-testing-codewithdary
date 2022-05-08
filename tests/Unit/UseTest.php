8eebbef343f8dd6bb2653bd72c88053ddc8718b9
Arvic S. Babol <kulotsystems@gmail.com>
2022-05-08 12:48:40 +0800

    Performed unit tests.

--- tests/Unit/UseTest.php
+++ tests/Unit/UseTest.php
@@ -2,17 +2,59 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
 
 class UseTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test if login route exist
      *
      * @return void
      */
-    public function test_example()
+    public function test_login_form()
     {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test if users can't have the same name and email
+     *
+     * @return void
+     */
+    public function test_user_duplication()
+    {
+        $user1 = User::make([
+            'name'  => 'John Doe',
+            'email' => 'johndoe@gmail.com'
+        ]);
+
+        $user2 = User::make([
+            'name'  => 'Dary',
+            'email' => 'dary@gmail.com'
+        ]);
+
+        $this->assertTrue($user1->name != $user2->name);
+    }
+
+
+    /**
+     * Test if user can be deleted.
+     *
+     * @return void
+     */
+    public function test_delete_user()
+    {
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user) {
+            $user->delete();
+        }
+
         $this->assertTrue(true);
     }
 }
